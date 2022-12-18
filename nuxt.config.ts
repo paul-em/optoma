5d5a361b7fcdbb8d5d9a2ff998f1c6a41816120c
Paul Em <paul3m@gmail.com>
2022-12-18 21:51:39 +0100

    setup gh pages deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
+import pkg from './package.json';
+
+// only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
+const routerPath = process.env.DEPLOY_ENV === 'GH_PAGES' ? `/${pkg.name}/` : '/';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   typescript: {
     strict: true
+  },
+  router: {
+    base: routerPath,
   }
 })
